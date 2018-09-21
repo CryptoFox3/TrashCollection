3afd55749a2154b6e00a3c37463deb00ec16f632
phodgson558 <pghodgson558@gmail.com>
2018-09-21 18:35:08 -0500

    Initial commit

---
+++ TrashCollection/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GarbageCollection.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int Zipcode { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ TrashCollection/Models/Pickup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GarbageCollection.Models
+{
+    public class Pickups
+    {
+        [Key]
+        public int PickupId { get; set; }
+        public string PickupWeekDay {get; set;}
+        public DateTime PickupDate { get; set; }
+
+        [ForeignKey("CustomerModels")]
+        public int CustomerId { get; set; }
+        public CustomerModels CustomerModels { get; set; }
+
+        public int Zipcode { get; set; }
+        public bool Repeat { get; set; }
+        public bool IsCompleted {get; set;}
+        public double PickupCost { get; set; }
+        
+    }
+}
